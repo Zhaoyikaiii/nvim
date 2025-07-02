e4c13a0b93552836a0418811b19ab42a9c67686d
Zhaoyikaiii <stryhytrgb@gmail.com>
2025-07-02 17:32:10 +0800

    chore: add show relative row number

--- lua/options.lua
+++ lua/options.lua
@@ -7,6 +7,10 @@ o.shiftwidth = 4
 o.tabstop = 4
 o.softtabstop = 4
 
+-- Row number
+o.relativenumber = true
+o.number = true
+
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
 -- set filetype for .CBL COBOL files.
