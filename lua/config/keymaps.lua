d0f8a813c8dede871ea4f31f80e8b6ed9bce2d88
Zhaoyikaiii <stryhytrgb@gmail.com>
2026-04-08 09:25:52 +0800

    feat: add LSP rename keymap for refactoring

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- LSP Rename
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
