bc9ef006799a3741ca3264e5f76c8fdcf0a5fd34
Zhaoyikaiii <stryhytrgb@gmail.com>
2026-04-14 17:21:44 +0800

    feat: add <leader>fG to resume last telescope picker

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -45,6 +45,7 @@ return {
     keys = {
       { "<leader>ff", "<cmd>Telescope find_files<cr>",             desc = "Find files" },
       { "<leader>fg", "<cmd>Telescope live_grep<cr>",              desc = "Live grep" },
+      { "<leader>fG", "<cmd>Telescope resume<cr>",                 desc = "Resume last picker" },
       { "<leader>fb", "<cmd>Telescope buffers<cr>",                desc = "Buffers" },
       { "<leader>fh", "<cmd>Telescope help_tags<cr>",              desc = "Help" },
       { "<leader>fr", "<cmd>Telescope oldfiles<cr>",               desc = "Recent files" },
